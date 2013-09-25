533713c7e62d65f73c8b11d50453cf42908333eb
Jonas Hinge <jonas.hinge@gmail.com>
2013-09-25 20:33:39 +0200

    working algorithm with correct score on TOY input file

--- Homework4/sequence_alignment_jhin.py
+++ Homework4/sequence_alignment_jhin.py
@@ -19,6 +19,9 @@ def get_score(char_x,char_y):
 	#score = 0
 	return score
 
+def get_char_from_sequence(seq,index):
+	return seq[index]
+
 
 
 # Sequence algorithm
@@ -27,22 +30,41 @@ def alignment(X,Y):
 	# gap penalty
 	delta = get_score(X[0],"*")
 
-	#M = [[None for x in range(len(X)+1)] for y in range(len(Y)+1)]
-	#A = [[None for x in range(len(X))] for y in range(len(Y))]
-	#for i in range(len(X)+1): M[0][i] = 0
-	#for j in range(len(Y)+1): M[j][0] = 0
-
-	for j in range(len(Y)+1): M[j][0] = delta*j
 	for i in range(len(X)+1): M[0][i] = delta*i
-	#print M
+	for j in range(len(Y)+1): M[j][0] = delta*j
+	
+	# build up M
 	for j in range(1,len(Y)+1):
 		for i in range(1,len(X)+1):
 			alpha = get_score(X[i-1],Y[j-1])
 			M[j][i] = max(alpha + M[j-1][i-1], delta + M[j][i-1], delta + M[j-1][i])
 
-	print M
+	# traceback for correct alignment
+	x_sequence = y_sequence = ""
+	i = len(X)
+	j = len(Y)
+	while(i > 0 and j > 0):
+		max_score = max(M[j][i-1], M[j-1][i], M[j-1][i-1])
+		print i, j
+		print M[j][i]
+		# horizontal
+		if(M[j][i-1] == max_score):
+			i -= 1
+			x_sequence = X[i] + x_sequence
+			y_sequence = "-" + y_sequence
+		# vertical
+		elif(M[j-1][i] == max_score):
+			j -= 1
+			x_sequence = "-" + x_sequence
+			y_sequence = Y[j] + y_sequence
+		else:
+			i -= 1
+			j -= 1
+			x_sequence = X[i] + x_sequence
+			y_sequence = Y[j] + y_sequence
 
-	return M[len(Y)][len(X)]
+
+	return M, M[len(Y)][len(X)], x_sequence, y_sequence
 
 
 
@@ -71,6 +93,9 @@ for line in score_file:
 			score_matrix.append(row)
 
 
-
-print alignment("KQRK","KQRIKAAKABK")
+table,score,x_align,y_align = alignment("KQRIKAAKABK","KAK")
+print table
+print score
+print x_align
+print y_align
 
