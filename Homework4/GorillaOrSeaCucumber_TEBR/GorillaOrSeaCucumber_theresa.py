b6e6c61d948633c9655594d0b7b83684131b1780
traysa <tebr@itu.dk>
2013-09-26 00:19:56 +0200

    Update: - Parsing complete - Aligment algorithm working ( O(m*n) ) - Space-Efficint-Algorithm working ( O(m+n)) - Bachward Space-Efficint-Algorithm NOT working - Divide-and-Conquer-Alignment algorithm NOT finished

--- Homework4/GorillaOrSeaCucumber_TEBR/GorillaOrSeaCucumber_theresa.py
+++ Homework4/GorillaOrSeaCucumber_TEBR/GorillaOrSeaCucumber_theresa.py
@@ -53,8 +53,8 @@ def parseScoreMatrix(filename):
                 if (len(scores) != len(proteins)):
                     print "ERROR: Matrix is corrupt"
                 proteinScores = {}
-                for num in range(0,len(scores)-1):
-                    proteinScores[proteins[num]] = scores[num]
+                for num in range(0,len(scores)):
+                    proteinScores[proteins[num]] = int(scores[num])
                 #print "ProteinScores: " + str(proteinScores)
                 scoreMatrix[protein[0]] = proteinScores
             #print "ScoreMatrix: " + str(scoreMatrix)
@@ -65,35 +65,128 @@ def parseInputFile(filename):
     proteinStructures = {}
     creaturePattern = '[^\>][\w\-]*'
     for line in inputFile:
-        #print line
         if (line.startswith(">")):
             creature = re.findall(creaturePattern,line)[0]
             #print "Creature: " +  str(creature)
+            proteinStructures[creature] = ""
         else:
-            proteins = re.findall("([A-Z])",line.strip())
+            proteins = re.findall("([A-Z]*)",line.strip())
             #print "Proteins: " + str(proteins)
-            proteinStructures[creature] = proteins
+            proteinStructures[creature] += str(proteins[0])
     return proteinStructures
-    
 
-# Read parameters and input files
+def Alignment(X,Y):
+    # Initialize
+    if ((len(Y) > 0) and (len(X) > 0)):
+        A = [[0 for i in range(len(X)+1)] for j in range(len(Y)+1)]
+        for i in range(len(X)+1):
+            A[0][i] = scoreMatrix[X[i-1].upper()]["*"]*i
+        for j in range(len(Y)+1):
+            A[j][0] = scoreMatrix[Y[j-1].upper()]["*"]*j
+        print A
+    # Minimum alignment cost
+        for j in range(1,len(Y)+1):
+            for i in range(1,len(X)+1):
+                A[j][i] = max(scoreMatrix[X[i-1].upper()][Y[j-1].upper()]+A[j-1][i-1],
+                            scoreMatrix[X[i-1].upper()]["*"]+A[j-1][i],
+                            scoreMatrix["*"][Y[j-1].upper()]+A[j][i-1])
+    else:
+        return 0
+    return A[len(Y)][len(X)]
 
-def algorithm():
-    # To-Do
-    return 0
+
+def SpaceEfficientAlignment(X,Y):
+    # Initialize
+    if ((len(Y) > 0) and (len(X) > 0)):
+        B = [[0 for i in range(len(X)+1)] for j in range(2)]
+        for i in range(len(X)+1):
+            B[0][i] = scoreMatrix[X[i-1].upper()]["*"]*i
+    # Minimum alignment cost
+        for j in range(1,len(Y)+1):
+            B[1][0] = scoreMatrix[Y[j-1].upper()]["*"]*j
+            for i in range(1,len(X)+1):
+                B[1][i] = max(scoreMatrix[X[i-1].upper()][Y[j-1].upper()]+B[0][i-1],
+                            scoreMatrix[X[i-1].upper()]["*"]+B[1][i-1],
+                            scoreMatrix["*"][Y[j-1].upper()]+B[0][i])
+            for i in range(0,len(X)+1):
+                B[0][i] = B[1][i]
+    else:
+        return 0
+    return B[0][len(X)]
+
+# Not working
+def BackwardSpaceEfficientAlignment(X,Y):
+    # Initialize
+    if ((len(Y) > 0) and (len(X) > 0)):
+        B = [[0 for i in range(len(X)+1)] for j in range(2)]
+        for i in range(len(X),-1,-1):
+            B[0][i] = scoreMatrix[X[i-1].upper()]["*"]*i
+        print B
+    # Minimum alignment cost
+        for j in range(len(Y)-1,-1,-1):
+            B[1][0] = scoreMatrix[Y[j-1].upper()]["*"]*j
+            for i in range(len(X)-1,-1,-1):
+                #print "i: " + str(i)
+                B[1][i] = max(scoreMatrix[X[i].upper()][Y[j].upper()]+B[0][i+1],
+                            scoreMatrix[X[i].upper()]["*"]+B[1][i+1],
+                            scoreMatrix["*"][Y[j].upper()]+B[0][i])
+            for i in range(len(X),-1,-1):
+                print "-" + str(B)
+                B[0][i] = B[1][i]
+                print B
+    else:
+        return 0
+    return B[1][1]
+
+#Not finished
+#def DivideAndConquerAlignment(X,Y):
+#    # To-Do
+#    if (len(X) <= 2 or len(Y) <= 2):
+#        return Alignment(X,Y)
+#    SpaceEfficientAlignment(X,Y[1:len(Y)/2])
+#    return 0
 
 if ((len(sys.argv) != 4)):
     printUsage()
     exit()
 else:
     scoreMatrixFile = [os.path.join(os.path.dirname(__file__), sys.argv[2])][0]
-    #print scoreMatrixFile
     scoreMatrix = parseScoreMatrix(scoreMatrixFile)
     print scoreMatrix
     
     inputFile = [os.path.join(os.path.dirname(__file__), sys.argv[1])][0]
-    #print inputFile
     proteinStructures = parseInputFile(inputFile)
     print proteinStructures
     
-    algorithm()
+    # Output
+    results = {}
+    creatures = proteinStructures.keys()
+    for c1 in range(len(creatures)):
+        for c2 in range(len(creatures)):
+        #print proteinStructures[creatures[c]]
+        #print proteinStructures[creatures[c+1]]
+            if (c1 != c2):
+                if ((str(creatures[c1]) + "--" + str(creatures[c2])) not in results and (str(creatures[c2]) + "--" + str(creatures[c1])) not in results):
+                    result = SpaceEfficientAlignment(proteinStructures[creatures[c1]],proteinStructures[creatures[c2]])
+                    results[str(creatures[c2]) + "--" + str(creatures[c1])] = str(result)
+                    results[str(creatures[c1]) + "--" + str(creatures[c2])] = str(result)                   
+    
+    # Compare results
+    filename = [os.path.join(os.path.dirname(__file__), sys.argv[3])][0]
+    for result in results:
+        line = result + ": " + results[result]
+        if line[:-4] in open(filename).read():
+            if line in open(filename).read():
+                print line + "\t\t--> OK"
+            else:
+                print line + "\t\t--> ERROR"
+    
+    '''
+    print BackwardSpaceEfficientAlignment("KQRK","KAK")
+    print BackwardSpaceEfficientAlignment("KAK","KQRK")
+    print BackwardSpaceEfficientAlignment("KQRIKAAKABK","KAK")
+    print BackwardSpaceEfficientAlignment("KAK","KQRIKAAKABK")
+    print BackwardSpaceEfficientAlignment("KQRK","KQRIKAAKABK")
+    print BackwardSpaceEfficientAlignment("KQRIKAAKABK","KQRK")
+    '''
+    
